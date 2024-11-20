db6ac0acc994f05f39a0c99cb3f518f2ba1e804c
helen-bernal <helen.bernal@globant.com>
2024-11-20 11:37:51 -0500

    Dependencies problem solved

--- src/test/java/com/globant/myTest/utils/test/BaseTest.java
+++ src/test/java/com/globant/myTest/utils/test/BaseTest.java
@@ -1,5 +1,18 @@
 package com.globant.myTest.utils.test;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.Properties;
+
 public class BaseTest {
-    private static String
+    private static String PROPERTIES_FILE="src/test/java/Resources/confirg.properties";
+    private static Properties properties= new Properties();
+    public static AndroidDriver driver;
+    @BeforeMethod(alwaysRun=true)
+    public void enviromentSetuo(){
+        UiAutomator2Options capabilities=new UiAutomator2Options();
+    }
+
 }
