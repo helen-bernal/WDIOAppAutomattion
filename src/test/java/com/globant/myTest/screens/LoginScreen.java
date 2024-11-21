a6cc831f579485022a6b59030e061b80f4f222b1
helen-bernal <helen.bernal@globant.com>
2024-11-20 22:08:44 -0500

    login Test

--- src/test/java/com/globant/myTest/screens/LoginScreen.java
+++ src/test/java/com/globant/myTest/screens/LoginScreen.java
@@ -11,51 +11,64 @@ public class LoginScreen extends BaseScreen {
     public LoginScreen(AndroidDriver driver) {
         super(driver);
     }
-    private static final String uiAutomator_LoginSingUp="new UiSelector().text(\"Login / Sign up Form\")";
-    @AndroidFindBy(uiAutomator =uiAutomator_LoginSingUp)
+
+    private static final String uiAutomator_LoginSingUp = "new UiSelector().text(\"Login / Sign up Form\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_LoginSingUp)
     private WebElement LoginSingUpText;
-    public Boolean isLoginSingUpTextPresent(){
+
+    public Boolean isLoginSingUpTextPresent() {
         return isELementDisplayed(LoginSingUpText);
     }
-    private static final String uiAutomator_LoginButton="new UiSelector().description(\"Login\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_LoginButton)
+
+    private static final String uiAutomator_LoginButton = "new UiSelector().description(\"Login\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_LoginButton)
     private WebElement LoginButton;
+
     public void clickLoginButton() {
         clickElement(LoginButton);
     }
-    private static final String uiAutomator_SingUpOption="new UiSelector().description(\"button-sign-up-container\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_SingUpOption)
+
+    private static final String uiAutomator_SingUpOption = "new UiSelector().description(\"button-sign-up-container\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_SingUpOption)
     private WebElement SingUpOption;
+
     public void clickSingUpOption() {
         clickElement(SingUpOption);
     }
-    private static final String uiAutomator_Email="new UiSelector().text(\"Email\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_Email)
+
+    private static final String uiAutomator_Email = "new UiSelector().text(\"Email\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_Email)
     private WebElement emailField;
-    private static final String uiAutomator_Password="new UiSelector().text(\"Password\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_Password)
+    private static final String uiAutomator_Password = "new UiSelector().text(\"Password\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_Password)
     private WebElement passwordField;
-    private static final String uiAutomator_ConfirmPassword="new UiSelector().text(\"Confirm password\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_ConfirmPassword)
+    private static final String uiAutomator_ConfirmPassword = "new UiSelector().text(\"Confirm password\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_ConfirmPassword)
     private WebElement confirmPasswordField;
-    private static final String uiAutomator_SignUpButton="new UiSelector().description(\"button-SIGN UP\")";
-    @AndroidFindBy(uiAutomator=uiAutomator_SignUpButton)
+    private static final String uiAutomator_SignUpButton = "new UiSelector().description(\"button-SIGN UP\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_SignUpButton)
     private WebElement SignUpButton;
+
     public void enterEmail(String email) {
         emailField.sendKeys(email);
     }
+
     public void enterPassword(String password) {
         passwordField.sendKeys(password);
     }
+
     public void confirmPassword(String password) {
         confirmPasswordField.sendKeys(password);
     }
+
     public void clickSignUpButton() {
         SignUpButton.click();
     }
-    private static final String uiAutomator_SuccessSignUo="new UiSelector().className(\"android.widget.FrameLayout\").instance(0)";
-    @AndroidFindBy(uiAutomator=uiAutomator_SuccessSignUo)
+
+    private static final String uiAutomator_SuccessSignUo = "new UiSelector().className(\"android.widget.FrameLayout\").instance(0)";
+    @AndroidFindBy(uiAutomator = uiAutomator_SuccessSignUo)
     private WebElement successText;
+
     public boolean isSuccessMessageDisplayed() {
         return successText.isDisplayed();
     }
@@ -81,4 +94,14 @@ public class LoginScreen extends BaseScreen {
         confirmPassword(password);
         clickSignUpButton();
     }
+
+    private static final String uiAutomator_SuccessLogin = "new UiSelector().className(\"android.widget.FrameLayout\").instance(0)";
+    @AndroidFindBy(uiAutomator = uiAutomator_SuccessLogin)
+    private WebElement successLoginText;
+
+    private static final String uiAutomator_UserLoginButton = "new UiSelector().description(\"button-LOGIN\")";
+    @AndroidFindBy(uiAutomator = uiAutomator_UserLoginButton)
+    private WebElement successLoginButtom;
 }
+
+
--- src/test/java/com/globant/myTest/test/LoginTest.java
+++ src/test/java/com/globant/myTest/test/LoginTest.java
@@ -1,11 +1,22 @@
 package com.globant.myTest.test;
 
+import com.globant.myTest.screens.LoginScreen;
 import com.globant.myTest.utils.test.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
     @Test
-    public void SuccesfulLogin(){
-
+    public void SuccessfulLogin(){
+        LoginScreen loginScreen= loginScreen();
+        loginScreen.clickLoginButton();
+        Assert.assertTrue(loginScreen.isLoginSingUpTextPresent());
+        String existingEmail = "h@gmail.com";
+        String existingPassword = "hh222333";
+        loginScreen.enterEmail(existingEmail);
+        loginScreen.enterPassword(existingPassword);
+        loginScreen.clickLoginButton();
+        Assert.assertTrue(loginScreen.isSuccessMessageDisplayed(), "Login was not successful.");
     }
+
 }
